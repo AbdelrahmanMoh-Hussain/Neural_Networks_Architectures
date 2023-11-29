81046fc2214c36e95f815b5bd1fe44ae36e336bf
Abdelrahman-Eldemerdash <abdelrahman.eldemerdash1@gmail.com>
2023-11-29 14:47:05 +0200

    task2 test added

--- Backpropagation.py
+++ Backpropagation.py
@@ -5,7 +5,7 @@ from preprocessing import *
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import seaborn as sns
-def propagation(number_of_hidden_layers, number_of_neurons,learing_rate, epochs, bias,function):
+def propagation(number_of_hidden_layers, number_of_neurons,learing_rate, epochs,bias,function):
     df = pd.read_excel('Dry_Bean_Dataset.xlsx')
     y = df['Class']
     x = df.drop(columns=['Class'])
@@ -22,27 +22,69 @@ def propagation(number_of_hidden_layers, number_of_neurons,learing_rate, epochs,
     neurons_count.append(3)
     # print(neurons_count)
     weights = [np.random.randn(neurons_count[i+1], neurons_count[i])*0.01 for i in range(num_layers - 1)]
-    biases = [np.random.randn(1, neurons_count[i + 1])*0.01 for i in range(num_layers - 1)]
+    if(bias):
+        biases = [np.random.randn(1, neurons_count[i + 1])*0.01 for i in range(num_layers - 1)]
+    else:
+        biases = [np.random.randn(1, neurons_count[i + 1]) * 0.0 for i in range(num_layers - 1)]
     # print(weights[0][0]) first dimension is for the layer weights[0] will get the weights for all neurons in this layer
     # weights[0][0] gets the weights for the first neuron in first layer
     # print(biases[0][0][1]) first dimension for layer, second is constant with 0 always, third for neuron
     x_arr = x.to_numpy()
     y_arr = y.to_numpy()
-    X_train, X_test, y_train, y_test = train_test_split(x_arr, y_arr, test_size=40, stratify=y, random_state=42)
-    train(neurons_count, num_layers, weights, biases, X_train, y_train, epochs)
+    print(weights)
+    print(biases)
+    X_train, X_test, y_train, y_test = train_test_split(x_arr, y_arr, test_size=60, stratify=y, random_state=42)
 
-def train(neurons_count,layers_count,weights,biases,x,y,epoch):
+    new_weights,new_biases=train(neurons_count, num_layers, weights, biases, X_train, y_train, epochs,function)
+    print(new_weights)
+    print(new_biases)
+    test(new_weights,new_biases,X_test,y_test,function)
+
+
+def train(neurons_count,layers_count,weights,biases,x,y,epoch,function):
     samples = x.shape[0]
     for e in range(epoch):
+        error =0
         for i in range(samples):
-            z=forward(weights,biases,x[i])
-            #print(z)
-            backward(weights,biases,layers_count,neurons_count,epoch,0,z,x[i],y[i], 5)
-            break
-        break
+            z=forward(weights,biases,x[i],function)
+            weights,biases=backward(weights,biases,layers_count,neurons_count,epoch,0,z,x[i],y[i], 5)
+            res = z[-1]
+            max = -10000000
+            for j in z[-1]:
+                if j > max:
+                    max = j
+            if max == res[0]:
+                y_pred = [1, 0, 0]
+            elif max == res[1]:
+                y_pred = [0, 1, 0]
+            else:
+                y_pred = [0, 0, 1]
+            if y_pred != y[i]:
+                error += 1
+    print("Train Accuracy : ",((samples-error)/samples)*100," %")
+    return weights,biases
+def test(weights,biases,x,y,function):
+    samples = x.shape[0]
+    print(samples)
+    error=0
+    for i in range(samples):
+        z=forward(weights,biases,x[i],function)
+        res=z[-1]
+        max=-10000000
+        for j in z[-1]:
+            if j>max:
+                max=j
+        if max==res[0]:
+            y_pred=[1, 0, 0]
+        elif max==res[1]:
+            y_pred = [0, 1, 0]
+        else:
+            y_pred = [0, 0, 1]
+        if y_pred!=y[i]:
+            error+=1
+    print("Test Accuracy : ",((samples-error)/samples)*100," %")
 
-
-def forward(weights, biases, sample):
+def forward(weights, biases, sample,fun):
     z=[]
     for i in range(len(weights)):
         z.append([])
@@ -51,8 +93,8 @@ def forward(weights, biases, sample):
                 net=np.dot(weights[i][j],sample)+biases[i][0][j]
             else:
                 net = np.dot(weights[i][j], z[i-1]) + biases[i][0][j]
-            sig=1 / (1 + np.exp(-net))
-            z[i].append(sig)
+            net=activation_fun(net,fun)
+            z[i].append(net)
     return z
 
 def backward(weights, biases, layers_count, neurons_count,epochs ,activation_function,z,x,y, learning_rate):
@@ -63,38 +105,46 @@ def backward(weights, biases, layers_count, neurons_count,epochs ,activation_fun
                 error[layer].append((y[neuron] - z[layer - 1][neuron]) * derivative(z[layer - 1][neuron], activation_function))
 
             elif layer == 0: #update weights
-                update_weights(weights, error, learning_rate, neurons_count, z, x)
+                new_weights,new_biases=update_weights(weights, biases,error, learning_rate, neurons_count, z, x)
                 break
-
             else: #1st case: [[ f`(net) * sum(error[k]*weight[k][j] ]]
                 derivative_func = derivative(z[layer - 1][neuron], activation_function)
                 summation = 0
                 for k in range(0,neurons_count[layer + 1]):
                     summation += error[layer + 1][k] * weights[layer][k][neuron]
                 error[layer].append(derivative_func * summation)
-
+    return new_weights,new_biases
     #forward(weights, biases, x)
 
-def update_weights(weights_list, error_list, learning_rate, neurons_count,z,x):
-    print("Before")
-    print(weights_list)
-    print(z)
-    for layer_index in range(0, 3):
-        print("Layer # " + str(layer_index))
+def update_weights(weights_list,biases, error_list, learning_rate, neurons_count,z,x):
+    # print("Before")
+    # print(weights_list)
+    # print(z)
+    for layer_index in range(0, len(neurons_count)-1):
+        # print("Layer # " + str(layer_index))
         for i in range(neurons_count[layer_index]):
-            print("i = " + str(i))
+            # print("i = " + str(i))
             for k in range(0, neurons_count[layer_index + 1]):
-                print("K = " + str(k))
+                # print("K = " + str(k))
                 if layer_index == 0:
-                    print("HERE X = " + str(x[i]))
+                    # print("HERE X = " + str(x[i]))
                     weights_list[layer_index][k][i] += learning_rate * error_list[layer_index + 1][k] * x[i]
+                    if biases[layer_index][0][k]!=0:
+                        biases[layer_index][0][k]+=learning_rate * error_list[layer_index + 1][k]
                 else:
                     weights_list[layer_index][k][i] += learning_rate * error_list[layer_index + 1][k] * z[layer_index - 1][i]
-        print(weights_list)
+                    if biases[layer_index][0][k]!=0:
+                        biases[layer_index][0][k]+=learning_rate * error_list[layer_index + 1][k]
+    return weights_list,biases
 def derivative(value, activation_function):
     if activation_function == 0:
         return value * (1 - value)
     else:
         return 1 - pow(math.tanh(value),2)
+def activation_fun(value, activation_function):
+    if activation_function == 0:
+        return 1 / (1 + np.exp(-value))
+    else:
+        return math.tanh(value)
 
-propagation(2,[3,4],0.1,1000,1,1)
+propagation(1,[3],0.01,1000,1,0)#sigmoid test
